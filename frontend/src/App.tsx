2eb83058c63286d1fe27e52efe10013516dba509
Paulo Mariano <paulomariano2501@gmail.com>
2022-07-26 23:05:44 -0300

    Add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,9 @@ import './styles.css';
 function App() {
   return (
     <div className="App">
-     <p>Teste</p>
+     <div className="header-container">
+      <img src="https://user-images.githubusercontent.com/78872071/135571859-726338e7-81f2-4e49-949d-8609de495c87.PNG" id='header-image'/>
+     </div>
     </div>
   );
 }
